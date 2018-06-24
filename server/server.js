acb7216f7a6608a2d1076f1a48fec6a564b9419f
Niskarsh Kumar <niskarshk@gmail.com>
2018-06-25 02:44:49 +0530

    lec

--- server/server.js
+++ server/server.js
@@ -10,4 +10,26 @@ class wild extends animal {
         console.log (`think`)
     }
 
+    letCheck () {
+        try {
+        
+        {
+            var a=1
+            let b=2
+            console.log (`a is ${a} b is ${b}`)
+
+        }
+
+        console.log (`a is ${a} b is ${b}`)
+    } catch (e) {
+        console.log (`error is ${e}`)
+    }
+
+    }
+
 }
+
+new wild('hi').isWorking()
+let nup = new wild ('hi')
+nup.isWorking()
+nup.letCheck()
