d0afe3a0029a97f4841905970843dcbd862ad4d4
Niskarsh Kumar <niskarshk@gmail.com>
2018-06-25 13:23:47 +0530

    final

--- resources/animal.js
+++ resources/animal.js
@@ -2,4 +2,8 @@ export default class animal {
     constructor (dog) {
         this.dog = dog
     }
+
+    isDog () {
+        console.log (`Dog name ${this.dog}`)
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import animal from '../resources/animal'
+import 'babel-polyfill'
 
 class wild extends animal {
     constructor (dog) {
@@ -7,8 +8,17 @@ class wild extends animal {
     }
 
     isWorking () {
+        let foo1
+        
         console.log (`think`)
+        let foo = data => {return data}
+        console.log (foo ('data'))
+        let a = () => {
+            this.letCheck();
+        }
     }
+
+    
 
     letCheck () {
         try {
@@ -16,11 +26,11 @@ class wild extends animal {
         {
             var a=1
             let b=2
-            console.log (`a is ${a} b is ${b}`)
+            console.log (`a is ${a} b is ${b}`)  //Guess what will be printed
 
         }
 
-        console.log (`a is ${a} b is ${b}`)
+        console.log (`a is ${a} b is ${b}`)  //Guess what will be printed
     } catch (e) {
         console.log (`error is ${e}`)
     }
@@ -29,7 +39,19 @@ class wild extends animal {
 
 }
 
-new wild('hi').isWorking()
-let nup = new wild ('hi')
-nup.isWorking()
-nup.letCheck()
+
+let nup = new wild ('hi') // creates object of class wild
+
+nup.isDog()  // Accessing function of class animal 
+nup.foo1 = 'hi foo'  
+console.log (` foo1 ${nup.foo1}`) //Guess what will be printed
+
+
+// different object of same class wild, using different argument for constructor method
+let second = new wild ('Tommy') 
+
+second.foo1 = 'foo2'  //same variable accessed from different object
+
+second.isDog()  //Guess what will be printed
+
+console.log (` foo2 ${second.foo1}`)  
