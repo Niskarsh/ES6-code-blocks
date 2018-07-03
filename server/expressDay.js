1e78b9dd78483d4abd433929f84b8d79efa7d36d
niskarsh <niskarshk@gmail.com>
2018-07-03 11:47:21 +0530

    Express day added

---
+++ server/expressDay.js
@@ -0,0 +1,30 @@
+// express module
+// server setting
+// nodemon
+// render and templeting
+// express.static
+// CRUD
+// request-response cycle
+// middlewares
+// forms
+
+// import express from 'express'
+// import hbs from 'hbs'
+
+// let app = express()
+// app.set ('view engine', hbs)
+// app.use (express.static(__dirname+'/../public'))
+
+// const port = process.env.PORT || 3000
+
+// app.get ('/', (req, res) => {
+//     res.render ('home.hbs')
+// })
+
+// app.get('/form', (req, res) => {
+//     res.render ('form.hbs')
+// })
+
+
+
+// app.listen (port, console.log (`Started listening on port ${port}`))
